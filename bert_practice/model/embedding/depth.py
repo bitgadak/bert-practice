1a8d1effe329ba94aa8901be0e35b4d1badba52d
bitgadak <bitgadak@gmail.com>
2022-04-08 22:04:11 +0900

    사이즈 증가

--- bert_practice/model/embedding/depth.py
+++ bert_practice/model/embedding/depth.py
@@ -4,4 +4,4 @@ import torch.nn as nn
 class DepthEmbedding(nn.Embedding):
 
     def __init__(self, embed_size=512):
-        super(DepthEmbedding, self).__init__(5, embed_size, padding_idx=0)
+        super(DepthEmbedding, self).__init__(6, embed_size, padding_idx=0)
